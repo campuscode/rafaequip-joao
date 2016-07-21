cbbb02a564099c09db3a29717fa46de25c822eed
Ariadyne e Diego <ariadyne.oliveira@yahoo.com.br>
2016-07-21 20:23:09 -0300

    Incluído mais cenários no teste

--- spec/features/user_list_price_spec.rb
+++ spec/features/user_list_price_spec.rb
@@ -1,19 +1,42 @@
 require 'rails_helper'
 
 feature 'User list all prices' do
-  scenario 'successfully' do
-
-    price = create(:price)
-    equipment = price.equipment
-    rental_period = price.rental_period
+  scenario 'successfully One equipment, two rental period' do
+    equipment = create(:equipment)
+    rental_period1, rental_period2 = create_list(:rental_period, 2)
+    price_1 = create(:price, equipment: equipment,
+                             rental_period: rental_period1)
+    price_2 = create(:price, equipment: equipment,
+                             rental_period: rental_period2)
 
     visit root_path
 
     click_on 'Listar Preços'
 
-    expect(page).to have_content equipment.name
-    expect(page).to have_content rental_period.period
-    expect(page).to have_content price.amount
+    expect(page).to have_content(equipment.name, count: 2)
+    expect(page).to have_content(price_1.rental_period.period)
+    expect(page).to have_content(price_2.rental_period.period)
+    expect(page).to have_content price_1.amount
+    expect(page).to have_content price_2.amount
+  end
+
+  scenario 'successfully Two equipment, one rental period' do
+    equipment1, equipment2 = create_list(:equipment, 2)
+    rental_period = create(:rental_period)
+    price_1 = create(:price, equipment: equipment1,
+                             rental_period: rental_period)
+    price_2 = create(:price, equipment: equipment2,
+                             rental_period: rental_period)
+
+    visit root_path
+
+    click_on 'Listar Preços'
+
+    expect(page).to have_content(equipment1.name)
+    expect(page).to have_content(equipment2.name)
+    expect(page).to have_content(rental_period.period, count: 2)
+    expect(page).to have_content(price_1.amount)
+    expect(page).to have_content(price_2.amount)
   end
 
   scenario 'fail' do
